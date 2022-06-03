192dc8fe4c72aa8211af3b4f0b8cda24813d48ec
Nick Mitchell <nickm@us.ibm.com>
2022-06-03 14:44:27 -0400

    fix: arm cask pointed to a non-existent url

--- Casks/codeflare.rb
+++ Casks/codeflare.rb
@@ -10,9 +10,9 @@ cask "codeflare" do
     sha256 "1e42811a5f16cb7eaa1c00c55b57ffa518460b731cc1bb7bec428a5155fa7f5d"
     app "CodeFlare-darwin-x64/CodeFlare.app"
   else
-    url "https://github.com/project-codeflare/codeflare-cli/releases/download/v#{version}/CodeFlare-darwin-amd64.tar.bz2"
+    url "https://github.com/project-codeflare/codeflare-cli/releases/download/v#{version}/CodeFlare-darwin-arm64.tar.bz2"
     sha256 "5135daed4b7ed304a0412fb2550ed4f534194b97e6f70d9ae2f3ba3daeb1cd37"
-    app "CodeFlare-darwin-amd64/CodeFlare.app"
+    app "CodeFlare-darwin-arm64/CodeFlare.app"
   end
 
   livecheck do
