8f2efe162ad5c4e7eb8094b95c9b5fd94694be83
Pablo Carmona <pablo.carmona.gonzalez@ibm.com>
2022-06-10 16:47:37 +0200

    Remove commented code. Add licensce to CurrentJob component.

--- plugins/plugin-client-default/src/components/CurrentJob.tsx
+++ plugins/plugin-client-default/src/components/CurrentJob.tsx
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 The Kubernetes Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React from 'react'
 import { Tab, getCurrentTab } from '@kui-shell/core'
 import { TextWithIconWidget, ViewLevel } from '@kui-shell/plugin-client-common'
@@ -34,15 +50,6 @@ export default class CurrentJob extends React.PureComponent<Props, State> {
     const currentJob = Object.keys(jobs).reverse()[0]
     this.setState({ currentJob })
   }
-
-  // handleCurrentJobClick() {
-  //   // const {
-  //   //   currentTab: { REPL: repl }
-  //   // } = this.state
-  //   // const response = await repl.qexec(`ray job logs ${this.state.currentJob}`)
-  //   console.log('handleCurrentJobClick')
-  //   console.log(this.state)
-  // }
 
   async componentWillMount() {
     await this.retrieveRunningJobs()
