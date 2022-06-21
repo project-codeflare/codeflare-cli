ca74160c2ddd4f137f40088585a41a501a45d18b
Nick Mitchell <nickm@us.ibm.com>
2022-06-21 10:29:48 -0400

    fix: sha256sums for 0.0.22 were swapped
    x86<->arm64

--- Casks/codeflare.rb
+++ Casks/codeflare.rb
@@ -7,11 +7,11 @@ cask "codeflare" do
 
   if Hardware::CPU.intel?
     url "https://github.com/project-codeflare/codeflare-cli/releases/download/v#{version}/CodeFlare-darwin-x64.tar.bz2"
-    sha256 "13c723e5e27d587690df3b68f252165034e71388f6df810732d4d3af7527d82d"
+    sha256 "d052f3366e0d5f10ef63d327f57b1247c192aca7a8673bf04ed5f5af8106a7ce"
     app "CodeFlare-darwin-x64/CodeFlare.app"
   else
     url "https://github.com/project-codeflare/codeflare-cli/releases/download/v#{version}/CodeFlare-darwin-arm64.tar.bz2"
-    sha256 "d052f3366e0d5f10ef63d327f57b1247c192aca7a8673bf04ed5f5af8106a7ce"
+    sha256 "13c723e5e27d587690df3b68f252165034e71388f6df810732d4d3af7527d82d"
     app "CodeFlare-darwin-arm64/CodeFlare.app"
   end
 
