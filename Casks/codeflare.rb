48b7d6443b2b4ade1ca29647c7020b46f4c1842f
Nick Mitchell <nickm@us.ibm.com>
2022-06-21 20:34:41 -0400

    chore: bump sha256sums to 0.0.25

--- Casks/codeflare.rb
+++ Casks/codeflare.rb
@@ -1,5 +1,5 @@
 cask "codeflare" do
-  version "0.0.24"
+  version "0.0.25"
 
   name "CodeFlare"
   desc "CLI for Project CodeFlare"
@@ -7,11 +7,11 @@ cask "codeflare" do
 
   if Hardware::CPU.intel?
     url "https://github.com/project-codeflare/codeflare-cli/releases/download/v#{version}/CodeFlare-darwin-x64.tar.bz2"
-    sha256 "12da5c6eaff1dee75a67b44f51a08ef8fd677f62d91e56d5c418b2f902b85662"
+    sha256 "4b240eaf84c2aa4181a4565572c0d9e3e76cf712da50983b94c0b171abd19288"
     app "CodeFlare-darwin-x64/CodeFlare.app"
   else
     url "https://github.com/project-codeflare/codeflare-cli/releases/download/v#{version}/CodeFlare-darwin-arm64.tar.bz2"
-    sha256 "ec3e27a59bc238e4149c2f35127a9ba154e0fedc6500f315f9a98db5d5bada20"
+    sha256 "ebd4af7f23df3e2bc1c0c8724e1e827c3d2ca755be306b9942ca160af95f1a17"
     app "CodeFlare-darwin-arm64/CodeFlare.app"
   end
 
