17fcd858378cca541d4ba01154506cb1d0d9d46a
Nick Mitchell <nickm@us.ibm.com>
2022-07-02 20:21:38 -0400

    feat: update event streaming UI to improve display of incremental torch progress
    the initial live progress added a UI element for every torch step. this PR refines that work to pre-fill UI elements for all epochs and iterations. it marks epochs as InProgress while that epoch's iterations are being processed. etc.
    
    this makes it appear more like a progress bar.

--- plugins/plugin-codeflare/src/controller/events/torch.ts
+++ plugins/plugin-codeflare/src/controller/events/torch.ts
@@ -20,18 +20,44 @@ type EventType = "Epoch" | "Iteration" | "Marker"
 type Detail = { epoch: number; step: number; nSteps: number; ip: string }
 export type TorchEvent = Event<EventType, Detail>
 
-function findPrevious(M: TorchEvent[], ip: TorchEvent["ip"], type: EventType) {
+function findPrevious(
+  M: TorchEvent[],
+  ip: TorchEvent["ip"],
+  type: EventType,
+  state: TorchEvent["state"],
+  step = -1,
+  epoch = -1
+) {
   for (let idx = M.length - 1; idx >= 0; idx--) {
     const evt = M[idx]
-    if (evt.type === type && evt.ip === ip) {
+    if (
+      evt.type === type &&
+      evt.ip === ip &&
+      evt.state === state &&
+      (step === -1 || evt.step === step) &&
+      (epoch === -1 || evt.epoch === epoch)
+    ) {
       return evt
     }
   }
 }
 
-function findEpoch(M: TorchEvent[], ip: TorchEvent["ip"]) {
-  const evt = findPrevious(M, ip, "Epoch")
-  return evt ? evt.step : -1
+function findEpoch(M: TorchEvent[], ip: TorchEvent["ip"], state: TorchEvent["state"] = "InProgress", step?: number) {
+  return findPrevious(M, ip, "Epoch", state, step)
+}
+
+class TorchEventImpl implements TorchEvent {
+  public constructor(
+    public readonly name: string,
+    public readonly ip: string,
+    public readonly type: EventType,
+    public readonly step: number,
+    public readonly nSteps: number,
+    public readonly epoch: number,
+    public readonly timestamp: number,
+    public state: TorchEvent["state"] = "InProgress",
+    public readonly message = `Epoch ${epoch}${type !== "Epoch" ? ` - ${type} ${step}` : ""} of ${nSteps}`
+  ) {}
 }
 
 export function collateEvent(M: TorchEvent[], line: string) {
@@ -46,7 +72,7 @@ export function collateEvent(M: TorchEvent[], line: string) {
     const epoch = -1
     const step = -1
     const nSteps = -1
-    const state = "InProgress"
+    const state = "Done"
     M.push({ ip, name, message, state, type, hidden, timestamp, epoch, step, nSteps })
     return M
   }
@@ -56,36 +82,63 @@ export function collateEvent(M: TorchEvent[], line: string) {
     const ip = match[1]
     const type = match[2] as EventType
     // const percentage = parseInt(match[3], 10)
-    const step = parseInt(match[4], 10)
+    const step = parseInt(match[4], 10) - (type === "Epoch" ? 0 : 1)
     const nSteps = parseInt(match[5], 10)
 
-    const epoch = type === "Epoch" ? step : findEpoch(M, ip)
-    const timestampMarker = findPrevious(M, ip, "Marker")
+    const epoch =
+      type === "Epoch"
+        ? { step, nSteps, state: "InProgress" }
+        : findEpoch(M, ip) || { step: -1, nSteps: 0, state: "InProgress" }
+    const name = `Torch Training on ${ip}`
+    const timestampMarker = findPrevious(M, ip, "Marker", "Done")
+    const timestamp = timestampMarker ? timestampMarker.timestamp : Date.now()
 
-    const event = {
-      name: `Torch Training on ${ip}`,
-      message: `Epoch ${epoch}${type !== "Epoch" ? ` - ${type} ${step}` : ""} of ${nSteps}`,
-      ip,
-      type,
-      step,
-      nSteps,
-      epoch,
-      timestamp: timestampMarker ? timestampMarker.timestamp : Date.now(),
-      state: "InProgress" as const,
-    }
-
-    // find previous by ip and mark it Done
-    const prev = findPrevious(M, ip, type)
-    if (prev) {
-      prev.state = "Done"
-
-      if (type === "Epoch" && prev.step === step) {
-        // strange, torch seems to repeat the e.g. Epoch 6/6 event...
-        return M
+    if (type === "Iteration") {
+      epoch.state = "InProgress"
+    } else if (step > 0) {
+      const thisEpoch = findEpoch(M, ip, "Pending", step)
+      if (thisEpoch) {
+        thisEpoch.state = "InProgress"
       }
     }
 
-    M.push(event)
+    // find previous by ip and mark it Done
+    if (step > 0) {
+      const prev =
+        type === "Iteration"
+          ? findPrevious(M, ip, type, "Pending", step - 1, epoch.step) // previous iteration in this epoch
+          : findPrevious(M, ip, type, "InProgress", step - 1, epoch.step - 1) // previous epoch
+      if (prev) {
+        prev.state = "Done"
+      } else if (type === "Iteration" && step === nSteps - 1) {
+        // torch repeat the last step to indicate fully done
+        const prev = findPrevious(M, ip, type, "Pending", nSteps - 1, epoch.step) // last iteration
+        if (prev) {
+          prev.state = "Done"
+        }
+      }
+    }
+
+    if (type === "Epoch" && step === 0) {
+      // first Epoch
+      M.push(new TorchEventImpl(name, ip, type, step, nSteps, epoch.step, timestamp))
+    } else if (type === "Iteration" && step === 0 && epoch.step === 0) {
+      // first Iteration of first Epoch: pre-fill the remaining Epochs and Iterations
+      M.push(new TorchEventImpl(name, ip, type, step, nSteps, epoch.step, timestamp, "Pending"))
+
+      // pre-fill remaining Iterations for epoch 0
+      for (let iterIdx = 1; iterIdx < nSteps; iterIdx++) {
+        M.push(new TorchEventImpl(name, ip, "Iteration", iterIdx, nSteps, 0, timestamp, "Pending"))
+      }
+
+      // now pre-fill the remaining Epochs
+      for (let epochIdx = 1; epochIdx < epoch.nSteps; epochIdx++) {
+        M.push(new TorchEventImpl(name, ip, "Epoch", epochIdx, epoch.nSteps, epochIdx, timestamp, "Pending"))
+        for (let iterIdx = 1; iterIdx < nSteps; iterIdx++) {
+          M.push(new TorchEventImpl(name, ip, "Iteration", iterIdx, nSteps, epochIdx, timestamp, "Pending"))
+        }
+      }
+    }
   }
 
   return M
