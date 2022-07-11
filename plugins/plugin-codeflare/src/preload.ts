9f68e59f950bd72b7d71dc487c7259231e9a7dd8
Pablo Carmona <pablo.carmona.gonzalez@ibm.com>
2022-07-11 18:36:27 +0200

    add initial tray bootstraping and placeholder image

---
+++ plugins/plugin-codeflare/src/preload.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2022 The Kubernetes Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export default async () => {
+  const { app, Menu, Tray } = await import("electron")
+
+  let tray = null
+
+  app.whenReady().then(() => {
+    tray = new Tray("/images/codeflare.png")
+    const contextMenu = Menu.buildFromTemplate([
+      { label: "Item1", type: "radio" },
+      { label: "Item2", type: "radio" },
+      { label: "Item3", type: "radio", checked: true },
+      { label: "Item4", type: "radio" },
+    ])
+    tray.setToolTip("CodeFlare")
+    tray.setContextMenu(contextMenu)
+  })
+}
