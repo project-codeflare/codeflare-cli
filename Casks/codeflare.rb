f4e6c33e0f78a1e577f2ce1c356addf61ddf8d1c
Nick Mitchell <nickm@us.ibm.com>
2022-07-22 09:23:53 -0400

    fix: sha256sums were incorrect for initial 0.4.4

--- Casks/codeflare.rb
+++ Casks/codeflare.rb
@@ -7,11 +7,11 @@ cask "codeflare" do
 
   if Hardware::CPU.intel?
     url "https://github.com/project-codeflare/codeflare-cli/releases/download/v#{version}/CodeFlare-darwin-x64.tar.bz2"
-    sha256 "bd97fc71ba99aa633230b0bc89c6d2d171c1454d35bb604620fc8900a3d43ea3"
+    sha256 "6f6d6bbb057afbfd1e030dcb7b4cbfe40f0c3d7aa41c5a0573a0922bbc152d74"
     app "CodeFlare-darwin-x64/CodeFlare.app"
   else
     url "https://github.com/project-codeflare/codeflare-cli/releases/download/v#{version}/CodeFlare-darwin-arm64.tar.bz2"
-    sha256 "6f6d6bbb057afbfd1e030dcb7b4cbfe40f0c3d7aa41c5a0573a0922bbc152d74"
+    sha256 "bd97fc71ba99aa633230b0bc89c6d2d171c1454d35bb604620fc8900a3d43ea3"
     app "CodeFlare-darwin-arm64/CodeFlare.app"
   end
 
