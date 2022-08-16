431b17ae2df961c896356cf97804efd0b243edd1
Nick Mitchell <nickm@us.ibm.com>
2022-08-16 08:46:04 -0400

    fix: log aggregator may fail in restricted docker hosts
    We are setting a USER in our Dockerfile that does not seem to work with containers secured by a SELinux host.

--- deploy/log-aggregator/wait-for.sh
+++ deploy/log-aggregator/wait-for.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 F="$1"
+D=$(dirname $F)
 
 # wait for the file to exist and be non-empty
 while [ ! -s "$F" ]; do
-    mkdir -p $(dirname $F)
-    inotifywait -qq -e create -e modify "$(dirname $F)"
+    if [ ! -d "$D" ]; then mkdir -p "$D" || break; fi
+    inotifywait -qq -e create -e modify "$D"
 done
+
+echo "Error in watch"
+exit 1
