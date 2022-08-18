17232c9a7c20ecf38771fcdfa0f6bef61abdc64e
Nick Mitchell <nickm@us.ibm.com>
2022-08-18 17:18:59 -0400

    test: for kind tests, place current profiles under non-gpu/ subdirectory

--- tests/kind/values.sh
+++ tests/kind/values.sh
@@ -4,4 +4,4 @@ SCRIPTDIR=$(cd $(dirname "$0") && pwd)
 ROOT="$SCRIPTDIR"/../../
 NODE=$(which node)
 CODEFLARE_HEADLESS_HOME="$ROOT"/dist/headless
-export MWPROFILES_PATH="$SCRIPTDIR"/profiles
+export MWPROFILES_PATH_BASE="$SCRIPTDIR"/profiles
