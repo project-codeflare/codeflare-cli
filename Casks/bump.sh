8474e8bf5bed83d89fafa32328fb93ce20388e8e
Nick Mitchell <nickm@us.ibm.com>
2022-08-23 20:49:56 -0400

    fix: release-it does not correctly publish cask updates
    We were using before:release, but it should be after:bump. Also, our cask bump.sh script was not templating in the new version.

--- Casks/bump.sh
+++ Casks/bump.sh
@@ -17,6 +17,7 @@ arm64=$(sha256sum "$SCRIPTDIR"/../dist/electron/CodeFlare-darwin-arm64.tar.bz2 |
 cp "$SCRIPTDIR"/codeflare.rb "$SCRIPTDIR"/codeflare.rb.bak
 
 cat "$SCRIPTDIR"/codeflare.tpl \
+    | sed -r "s/{{ version }}/$version/g" \
     | sed -r "s/{{ x64 }}/$x64/g" \
     | sed -r "s/{{ arm64 }}/$arm64/g" \
           > "$SCRIPTDIR"/codeflare.rb
