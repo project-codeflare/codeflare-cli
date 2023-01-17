85b22b0804093dffb37df9e3648ee12a1930b77d
Nick Mitchell <nickm@us.ibm.com>
2023-01-17 17:04:32 -0500

    Release 2.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## [2.2.1](https://github.com/project-codeflare/codeflare-cli/compare/v2.2.0...v2.2.1) (2023-01-17)
+
+### Bug Fixes
+
+- bump to madwizard@4.3.5 to pick up enquirer bump ([fdffb11](https://github.com/project-codeflare/codeflare-cli/commit/fdffb1116d954d90704b537c8838a3217604a546))
+
 # [2.2.0](https://github.com/project-codeflare/codeflare-cli/compare/v2.1.3...v2.2.0) (2023-01-17)
 
 ### Bug Fixes
--- Casks/codeflare.rb
+++ Casks/codeflare.rb
@@ -1,5 +1,5 @@
 cask "codeflare" do
-  version "2.2.0"
+  version "2.2.1"
 
   name "CodeFlare"
   desc "CLI for Project CodeFlare"
@@ -7,11 +7,11 @@ cask "codeflare" do
 
   if Hardware::CPU.intel?
     url "https://github.com/project-codeflare/codeflare-cli/releases/download/v#{version}/CodeFlare-darwin-x64.tar.bz2"
-    sha256 "fa75133ab98019fc968370a7e32a03d7acc97f63c4996fc23af715a092d9001a"
+    sha256 "f67e56fb3bd383c955394516b20ab18c1bcd28141216c39e6b28ac49cff6b97f"
     app "CodeFlare-darwin-x64/CodeFlare.app"
   else
     url "https://github.com/project-codeflare/codeflare-cli/releases/download/v#{version}/CodeFlare-darwin-arm64.tar.bz2"
-    sha256 "30f72c8b6fa1de15bd07563929828231b9b86231599e600eaef2fe1b39c47df7"
+    sha256 "7400750a0d89f2aa201434af38df9e91fd7979e102ec19b01b53585e35206653"
     app "CodeFlare-darwin-arm64/CodeFlare.app"
   end
 
