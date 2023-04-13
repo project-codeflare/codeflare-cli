cec4559f0c5a8fa9fab88f398e36c2ccfe3251cf
Nick Mitchell <nickm@us.ibm.com>
2023-04-12 20:04:13 -0400

    fix: revert to ink@3 due to weird codeflare db silent failures

--- plugins/plugin-codeflare-dashboard/src/components/Dashboard/Grid.tsx
+++ plugins/plugin-codeflare-dashboard/src/components/Dashboard/Grid.tsx
@@ -208,12 +208,11 @@ export default class Grid extends React.PureComponent<GridProps> {
     const flexDirection = this.legendPosition === "below" ? "column" : "row"
     const alignItems = this.legendPosition === "below" ? "center" : "center"
     const legendBoxProps = this.legendPosition === "below" ? { marginTop: 1 } : { marginLeft: 2 }
-    const flexWrap = this.legendPosition === "right" ? "wrap" : "nowrap"
+    // const flexWrap = this.legendPosition === "right" ? "wrap" : "nowrap"
 
     return (
       <Box
         flexDirection={flexDirection}
-        flexWrap={flexWrap}
         alignItems={alignItems}
         justifyContent="center"
         paddingTop={1}
